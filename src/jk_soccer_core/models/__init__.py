5924763a4d9226db7552c3e1ff785c660db42d40
Omar Crosby <omar.crosby@gmail.com>
2025-01-26 09:28:38 -0500

    chore: export models from the models package

--- src/jk_soccer_core/models/__init__.py
+++ src/jk_soccer_core/models/__init__.py
@@ -1,2 +1,9 @@
 from .person import Person
 from .thing import Thing
+from .place import Place
+
+from .coach import Coach
+from .event import Event
+from .match import Match
+from .player import Player
+from .team import Team
